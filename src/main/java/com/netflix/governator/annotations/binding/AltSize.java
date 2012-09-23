594a181ab0e884fffb81663d64fd27bc442ab71e
Jordan Zimmerman <jordan@jordanzimmerman.com>
2012-09-23 13:05:15 -0700

    Some generically named binding annotations

---
+++ src/main/java/com/netflix/governator/annotations/binding/AltSize.java
@@ -0,0 +1,17 @@
+package com.netflix.governator.annotations.binding;
+
+import com.google.inject.BindingAnnotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@BindingAnnotation
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+/**
+ * A generic binding annotation
+ */
+public @interface AltSize
+{
+}
