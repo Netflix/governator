9d5e21542045f111507165f7b1a2429c4a00fa81
Jordan Zimmerman <jordan@jordanzimmerman.com>
2012-09-23 13:05:09 -0700

    fixed the doc

--- src/main/java/com/netflix/governator/lifecycle/LifecycleListener.java
+++ src/main/java/com/netflix/governator/lifecycle/LifecycleListener.java
@@ -6,15 +6,14 @@ package com.netflix.governator.lifecycle;
 public interface LifecycleListener
 {
     /**
-     * When the IoC container being used injects an object, this callback will
-     * be notified
+     * When Guice injects an object, this callback will be notified
      *
      * @param obj object being injected
      */
     public void     objectInjected(Object obj);
 
     /**
-     * Called when an objects lifecycle state changes
+     * Called when an object's lifecycle state changes
      *
      * @param obj the object
      * @param newState new state
