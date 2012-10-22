50ad39b7d8eb5c4c4f914698857e62a8f1209e1e
Jordan Zimmerman <jordan@jordanzimmerman.com>
2012-10-21 20:51:48 -0700

    a bit of refactoring and some doc

--- src/main/java/com/netflix/governator/lifecycle/warmup/DependencyNode.java
+++ src/main/java/com/netflix/governator/lifecycle/warmup/DependencyNode.java
@@ -3,27 +3,27 @@ package com.netflix.governator.lifecycle.warmup;
 import java.util.LinkedList;
 import java.util.List;
 
-public class DependencyNode
+class DependencyNode
 {
     private final List<DependencyNode> children = new LinkedList<DependencyNode>();
     private final Object key;
 
-    public DependencyNode(Object key)
+    DependencyNode(Object key)
     {
         this.key = key;
     }
 
-    public void addChild(DependencyNode child)
+    void addChild(DependencyNode child)
     {
         children.add(child);
     }
 
-    public Object getKey()
+    Object getKey()
     {
         return key;
     }
 
-    public List<DependencyNode> getChildren()
+    List<DependencyNode> getChildren()
     {
         return children;
     }
