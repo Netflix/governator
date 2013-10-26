f56f010bedb5febcd214a5c8ede87c899a19386f
randgalt <jordan@jordanzimmerman.com>
2013-10-26 07:39:50 -0700

    Issue 82: DAG builder wasn't handling bindings through interfaces. Integrated pull request that builds implicit dependencies. Also, as a side benefit, the PR reduces the size of the DAG by removing non warmup deps.

--- src/main/java/com/netflix/governator/lifecycle/warmup/DAGManager.java
+++ src/main/java/com/netflix/governator/lifecycle/warmup/DAGManager.java
@@ -16,13 +16,19 @@
 
 package com.netflix.governator.lifecycle.warmup;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
+import com.google.common.reflect.TypeToken;
+import com.google.inject.TypeLiteral;
+import com.netflix.governator.annotations.WarmUp;
 import com.netflix.governator.lifecycle.LifecycleMethods;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -32,12 +38,12 @@ import java.util.Set;
  */
 public class DAGManager
 {
-    private final Map<Object, Object>               keyToObject = Maps.newHashMap();
-    private final Map<Object, LifecycleMethods>     keyToLifecycle = Maps.newHashMap();
-    private final Multimap<Object, Object>          dependencies = ArrayListMultimap.create();
-    private final Set<Object>                       nonRoots = Sets.newHashSet();
+    private final Map<Object, Object> keyToObject = Maps.newHashMap();
+    private final Map<Object, LifecycleMethods> keyToLifecycle = Maps.newHashMap();
+    private final Multimap<Object, Object> dependencies = ArrayListMultimap.create();
+    private final Set<Object> nonRoots = Sets.newHashSet();
 
-    private static final Object         ROOT_DEPENDENCY_KEY = new Object();
+    private static final Object ROOT_DEPENDENCY_KEY = new Object();
 
     /**
      * Return a copy of this DAGManager and then clears this instance
@@ -66,8 +72,8 @@ public class DAGManager
      * Adds a mapping of an object "key" to an object
      *
      * @param objectKey the object's key
-     * @param object the object
-     * @param methods the objects lifecycle methods
+     * @param object    the object
+     * @param methods   the objects lifecycle methods
      */
     public synchronized void addObjectMapping(Object objectKey, Object object, LifecycleMethods methods)
     {
@@ -79,7 +85,7 @@ public class DAGManager
     /**
      * Adds a dependency for the given object
      *
-     * @param objectKey object's key
+     * @param objectKey     object's key
      * @param dependencyKey object key of the dependency
      */
     public synchronized void addDependency(Object objectKey, Object dependencyKey)
@@ -94,6 +100,30 @@ public class DAGManager
      * @return root of the DAG
      */
     public synchronized DependencyNode buildTree()
+    {
+        processDependencies();
+        return generateTree();
+    }
+
+    public synchronized Object getObject(Object key)
+    {
+        return keyToObject.get(key);
+    }
+
+    public synchronized LifecycleMethods getLifecycleMethods(Object key)
+    {
+        return keyToLifecycle.get(key);
+    }
+
+    public synchronized void clear()
+    {
+        keyToObject.clear();
+        keyToLifecycle.clear();
+        dependencies.clear();
+    }
+
+    @VisibleForTesting
+    DependencyNode generateTree()
     {
         DependencyNode root = new DependencyNode(new Object());
         for ( Object objectKey : dependencies.keySet() )
@@ -110,26 +140,9 @@ public class DAGManager
         return root;
     }
 
-    public synchronized Object getObject(Object key)
-    {
-        return keyToObject.get(key);
-    }
-
-    public synchronized LifecycleMethods getLifecycleMethods(Object key)
-    {
-        return keyToLifecycle.get(key);
-    }
-
-    public synchronized void        clear()
-    {
-        keyToObject.clear();
-        keyToLifecycle.clear();
-        dependencies.clear();
-    }
-
     private DependencyNode internalBuildTree(DependencyNode parent, Object objectKey)
     {
-        DependencyNode      node = new DependencyNode(objectKey);
+        DependencyNode node = new DependencyNode(objectKey);
         if ( parent != null )
         {
             parent.addChild(node);
@@ -141,5 +154,115 @@ public class DAGManager
         }
 
         return node;
+    }
+
+    private void processDependencies()
+    {
+        addImplicitDependencies();
+        removeNonWarmUpDependencies();
+        checkForCircularDependencies();
+    }
+
+    private void addImplicitDependencies()
+    {
+        Set<Object> objects = Sets.newHashSet();
+        objects.addAll(dependencies.keySet());
+        objects.addAll(dependencies.values());
+        Set<List<Object>> pairs = Sets.cartesianProduct(objects, objects);
+        for ( List<Object> pair : pairs )
+        {
+            addImplicitDependency(pair.get(0), pair.get(1));
+        }
+    }
+
+    // This adds dependency entries indicating that an interface or superclass depends on its implementation(s).
+    private void addImplicitDependency(final Object parent, final Object child)
+    {
+        if ( (parent instanceof TypeLiteral<?>) && (child instanceof TypeLiteral<?>) && !parent.equals(child) )
+        {
+            TypeLiteral<?> parentType = (TypeLiteral<?>)parent;
+            TypeLiteral<?> childType = (TypeLiteral<?>)child;
+            if ( TypeToken.of(parentType.getType()).isAssignableFrom(childType.getType()) )
+            {
+                dependencies.put(parent, child);
+            }
+        }
+    }
+
+    /*
+     * The InternalLifecycleManager tells us about the dependencies in the entire object graph, but we're only concerned
+     * about dependencies as the effect warmUp methods so we're going to remove entries related to classes without warm
+     * up methods. We have to protect the dependencies of warm up methods though.
+     */
+    private void removeNonWarmUpDependencies()
+    {
+        for ( final Iterator<Object> it = dependencies.keySet().iterator(); it.hasNext(); )
+        {
+            final Object key = it.next();
+            if ( !hasWarmUpMethod(key) )
+            {
+                if ( extractDependency(key) )
+                {
+                    it.remove();
+                }
+            }
+        }
+        // we already removed anything wihout warmups from the keys, so this won't break any dependencies,
+        // just shrink the dependency map
+        for ( final Iterator<Object> it = dependencies.values().iterator(); it.hasNext(); )
+        {
+            final Object value = it.next();
+            if ( (!hasWarmUpMethod(value) && ROOT_DEPENDENCY_KEY != value) || dependencies.get(value).contains(value) )
+            {
+                it.remove();
+            }
+        }
+    }
+
+    private boolean extractDependency(final Object key)
+    {
+        boolean dependenciesPreserved = false;
+        final Collection<Object> values = dependencies.get(key);
+        for ( final Map.Entry<Object, Collection<Object>> entry : dependencies.asMap().entrySet() )
+        {
+            if ( entry.getValue().contains(key) )
+            {
+                dependenciesPreserved = true;
+                entry.getValue().addAll(values);
+            }
+        }
+        return dependenciesPreserved;
+    }
+
+    private boolean hasWarmUpMethod(final Object bridge)
+    {
+        final LifecycleMethods lifecycleMethods = keyToLifecycle.get(bridge);
+        return null != lifecycleMethods && !lifecycleMethods.methodsFor(WarmUp.class).isEmpty();
+    }
+
+    /*
+     * With the addition of implicit dependencies, circular dependencies are now possible.
+     */
+    private void checkForCircularDependencies()
+    {
+        final Set<Object> couldBeCircular = Sets.newHashSet(dependencies.keySet());
+        couldBeCircular.retainAll(dependencies.values());
+        checkForCircularDependencies(Sets.newLinkedHashSet(), couldBeCircular);
+    }
+
+    private void checkForCircularDependencies(final Set<Object> seen, final Collection<Object> objects)
+    {
+        for ( final Object object : objects )
+        {
+            if ( !seen.add(object) )
+            {
+                if ( hasWarmUpMethod(object) )
+                {
+                    throw new IllegalStateException("Circular dependency detected: " + seen + " -> " + object);
+                }
+            }
+            checkForCircularDependencies(seen, dependencies.get(object));
+            seen.remove(object);
+        }
     }
 }
