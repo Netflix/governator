8bd0b44c98be03082bbbe37511a019d12a530634
elandau <elandau@yahoo.com>
2014-05-14 22:57:30 -0700

    Fix unit test and add BindingReport action

---
+++ governator-core/src/main/java/com/netflix/governator/guice/actions/BindingReport.java
@@ -0,0 +1,37 @@
+package com.netflix.governator.guice.actions;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Maps;
+import com.google.inject.Binding;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.netflix.governator.guice.PostInjectorAction;
+
+public class BindingReport implements PostInjectorAction {
+    private static final Logger LOG = LoggerFactory.getLogger(BindingReport.class);
+            
+    private final String label;
+    
+    public BindingReport(String label) {
+        this.label = label;
+    }
+    
+    @Override
+    public void call(Injector injector) {
+        LOG.info("Bindings for " + label);
+        for (Entry<Key<?>, Binding<?>> binding : injector.getBindings().entrySet()) {
+            LOG.info("Explicit: " + binding.getKey());
+        }
+        
+        Map<Key<?>, Binding<?>> jitBindings = Maps.difference(injector.getAllBindings(), injector.getBindings()).entriesOnlyOnLeft();
+        
+        for (Entry<Key<?>, Binding<?>> binding : jitBindings.entrySet()) {
+            LOG.info("JIT: " + binding.getKey());
+        }
+    }
+}
