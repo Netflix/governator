6d4375d82c47bcf8db4a746a436de4b9af16df36
elandau <elandau@yahoo.com>
2014-07-14 14:14:08 -0700

    @GovernatorConfiguration bootstrap annotation
    The @GovernatorConfiguration can be used to configure core features of
    the LifecycleInjectorBuilder, such as turning off AutoBindSingleton.
    Added unit test example for using the bootstrap model in JUnit

---
+++ governator-test-junit/src/test/java/com/netflix/governator/guice/BootstrapExample.java
@@ -0,0 +1,47 @@
+package com.netflix.governator.guice;
+
+import junit.framework.Assert;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.google.inject.AbstractModule;
+import com.netflix.governator.guice.annotations.GovernatorConfiguration;
+
+public class BootstrapExample {
+    public static interface Foo {
+        
+    }
+    
+    public static class FooImpl implements Foo {
+        
+    }
+    
+    public static class FooSuite extends AbstractModule {
+        @Override
+        protected void configure() {
+            bind(Foo.class).to(FooImpl.class);
+        }
+    }
+    
+    @GovernatorConfiguration
+    public static class Configuration extends AbstractModule {
+        @Override
+        protected void configure() {
+            bind(String.class).toInstance("configuration");
+        }
+    }
+    
+    @Rule
+    public LifecycleTester tester = new LifecycleTester(Configuration.class);
+    
+    @Test
+    public void testFooSuiteInstalled() {
+        tester.withSuite(Suites.from(FooSuite.class));
+        tester.start();
+        
+        String foo = tester.getInstance(String.class);
+        Assert.assertEquals("configuration",  foo);
+        tester.getInstance(Foo.class);
+    }
+}
