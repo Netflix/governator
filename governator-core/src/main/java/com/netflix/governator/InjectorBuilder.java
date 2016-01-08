4cc1edd0e6e0a997141fc841116a339e0d9e4a15
elandau <elandau@yahoo.com>
2016-01-08 13:15:14 -0800

    Deprecate @Modules in favor of Guice's built-in deduper for installed modules

--- governator-core/src/main/java/com/netflix/governator/InjectorBuilder.java
+++ governator-core/src/main/java/com/netflix/governator/InjectorBuilder.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.inject.Binder;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
