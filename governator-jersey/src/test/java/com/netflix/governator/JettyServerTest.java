d139d240c602c6f2016f154f709f8e395ee974f8
elandau <elandau@netflix.com>
2016-02-25 19:24:08 -0800

    Revert "Initial commit of governator-jersey enhancements to jersey-guice integration"
    This reverts commit 22908793e3e9d2848cd029ca582c38ab63be8139.

--- governator-jersey/src/test/java/com/netflix/governator/JettyServerTest.java
+++ governator-jersey/src/test/java/com/netflix/governator/JettyServerTest.java
@@ -4,47 +4,34 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Map;
+
+import javax.annotation.PreDestroy;
+
+import junit.framework.Assert;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.testng.annotations.Test;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.inject.AbstractModule;
-import com.google.inject.CreationException;
 import com.google.inject.Provides;
 import com.google.inject.util.Modules;
 import com.netflix.governator.guice.jetty.DefaultJettyConfig;
-import com.netflix.governator.guice.jetty.GovernatorJerseyServletModule;
 import com.netflix.governator.guice.jetty.JettyConfig;
 import com.netflix.governator.guice.jetty.JettyModule;
-import com.netflix.governator.jersey.annotations.ServletContainerProperties;
-import com.sun.jersey.api.core.PackagesResourceConfig;
 
 public class JettyServerTest {
     @Test
-    public void confirmResourceLoadedWithoutBinding() throws InterruptedException, MalformedURLException, IOException {
+    public void confirmShutdownSequence() throws InterruptedException, MalformedURLException, IOException {
         // Create the injector and autostart Jetty
         LifecycleInjector injector = InjectorBuilder.fromModules(
+                new SampleServletModule(), 
                 new ShutdownHookModule(), 
-                new GovernatorJerseyServletModule() {
-                    @Override
-                    protected void configureMoreServlets() {
-                    }
-                    
-                    @Provides
-                    @ServletContainerProperties
-                    Map<String, Object> getContainerProperties() {
-                        return ImmutableMap.<String, Object>builder()
-                                .put(PackagesResourceConfig.PROPERTY_PACKAGES, "com.netflix")
-                                .build();
-                    }
-                },
                 Modules.override(new JettyModule())
                        .with(new AbstractModule() {
                             @Override
                             protected void configure() {
+                                bind(Foo.class).asEagerSingleton();
                             }
                             
                             @Provides
@@ -55,13 +42,18 @@ public class JettyServerTest {
                         }))
                         .createInjector();
 
+        Foo foo = injector.getInstance(Foo.class);
+        
         // Determine the emphermal port from jetty
         Server server = injector.getInstance(Server.class);
         int port = ((ServerConnector)server.getConnectors()[0]).getLocalPort();
         
-        System.out.println("Listening on port : "+ port);
+        SampleResource resource = injector.getInstance(SampleResource.class);
+        Assert.assertEquals(1, resource.getPostConstructCount());
+        Assert.assertEquals(0, resource.getPreDestroyCount());
         
-        URL url = new URL(String.format("http://localhost:%d/", port));
+        System.out.println("Listening on port : "+ port);
+        URL url = new URL(String.format("http://localhost:%d/kill", port));
         HttpURLConnection conn = (HttpURLConnection)url.openConnection();
         try {
             conn.getResponseCode();
@@ -69,89 +61,10 @@ public class JettyServerTest {
         catch (Exception e) {
             
         }
-        injector.shutdown();
-    }
-    
-    @Test(expectedExceptions={CreationException.class})
-    public void confirmFailedToCreateWithoutRootResources() throws InterruptedException, MalformedURLException, IOException {
-        // Create the injector and autostart Jetty
-        LifecycleInjector injector = InjectorBuilder.fromModules(
-                new ShutdownHookModule(), 
-                new GovernatorJerseyServletModule() {
-                    @Override
-                    protected void configureMoreServlets() {
-                    }
-                },
-                Modules.override(new JettyModule())
-                       .with(new AbstractModule() {
-                            @Override
-                            protected void configure() {
-                            }
-                            
-                            @Provides
-                            JettyConfig getConfig() {
-                                // Use emphemeral ports
-                                return new DefaultJettyConfig().setPort(0);
-                            }
-                        }))
-                        .createInjector();
-
-        // Determine the emphermal port from jetty
-        Server server = injector.getInstance(Server.class);
-        int port = ((ServerConnector)server.getConnectors()[0]).getLocalPort();
+        injector.awaitTermination();
         
-        System.out.println("Listening on port : "+ port);
-        
-        URL url = new URL(String.format("http://localhost:%d/", port));
-        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-        try {
-            conn.getResponseCode();
-        }
-        catch (Exception e) {
-            
-        }
-        injector.shutdown();
-    }
-    
-    @Test
-    public void confirmResourceLoadedWithBindingOnly() throws InterruptedException, MalformedURLException, IOException {
-        // Create the injector and autostart Jetty
-        LifecycleInjector injector = InjectorBuilder.fromModules(
-                new ShutdownHookModule(), 
-                new GovernatorJerseyServletModule() {
-                    @Override
-                    protected void configureMoreServlets() {
-                        bind(SampleResource.class);
-                    }
-                },
-                Modules.override(new JettyModule())
-                       .with(new AbstractModule() {
-                            @Override
-                            protected void configure() {
-                            }
-                            
-                            @Provides
-                            JettyConfig getConfig() {
-                                // Use emphemeral ports
-                                return new DefaultJettyConfig().setPort(0);
-                            }
-                        }))
-                        .createInjector();
-
-        // Determine the emphermal port from jetty
-        Server server = injector.getInstance(Server.class);
-        int port = ((ServerConnector)server.getConnectors()[0]).getLocalPort();
-        
-        System.out.println("Listening on port : "+ port);
-        
-        URL url = new URL(String.format("http://localhost:%d/", port));
-        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-        try {
-            conn.getResponseCode();
-        }
-        catch (Exception e) {
-            
-        }
-        injector.shutdown();
+        Assert.assertTrue(foo.shutdownCalled);
+        Assert.assertEquals(1, resource.getPostConstructCount());
+        Assert.assertEquals(1, resource.getPreDestroyCount());
     }
 }
--- governator-jersey/src/test/java/com/netflix/governator/SampleResource.java
+++ governator-jersey/src/test/java/com/netflix/governator/SampleResource.java
@@ -2,6 +2,8 @@ package com.netflix.governator;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
@@ -16,14 +18,52 @@ public class SampleResource {
     private static final Logger LOG = LoggerFactory.getLogger(SampleResource.class);
     
     private AtomicInteger counter = new AtomicInteger();
+    private final LifecycleShutdownSignal event;
+    private final AtomicInteger postConstruct = new AtomicInteger();
+    private final AtomicInteger preDestroy = new AtomicInteger();
     
     @Inject
     public SampleResource(LifecycleShutdownSignal event) {
+        this.event = event;
     }
     
     @GET
     public String getHello() {
         LOG.info("Saying hello");
         return "hello " + counter.incrementAndGet();
+    }
+    
+    @PostConstruct
+    public void init() {
+        postConstruct.incrementAndGet();
+        LOG.info("Post construct " + postConstruct.get());
+    }
+    
+    @PreDestroy
+    public void shutdown() {
+        preDestroy.incrementAndGet();
+        LOG.info("Pre destroy " + preDestroy.get());
+    }
+    
+    
+    @Path("kill")
+    public String kill() {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                event.signal();
+            }
+        });
+        t.setDaemon(true);
+        t.start();
+        return "killing";
+    }
+    
+    public int getPreDestroyCount() {
+        return preDestroy.get();
+    }
+    
+    public int getPostConstructCount() {
+        return postConstruct.get();
     }
 }
