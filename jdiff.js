b0ccbe7f527409562356cddf87d11c48ea9c3025
CloudBees DEV@Cloud <nobody@cloudbees.com>
2016-04-06 15:26:42 -0700

    Publish of Github pages from Gradle.

--- jdiff.js
+++ jdiff.js
@@ -2,6 +2,7 @@
 angular.module('Index', [])
   .controller('JDiff', ['$scope', function ($scope) {
     $scope['j-diff-reports'] = [
+        '1.11.0-to-1.12.11',
         '1.11.0-to-1.12.10'].map(function (jDiffReport) {
           return [jDiffReport.replace(/.*-to-/, ''), jDiffReport];
         });
