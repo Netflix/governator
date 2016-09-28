ebd080e5b6e8d91dcb292cf9b3f7ff303e3332ea
Eran Landau <elandau@netflix.com>
2016-09-27 18:45:44 -0700

    Update javadoc

--- governator-providers/src/main/java/com/netflix/governator/providers/ProvidesWithAdvice.java
+++ governator-providers/src/main/java/com/netflix/governator/providers/ProvidesWithAdvice.java
@@ -10,26 +10,26 @@ import java.lang.annotation.Target;
 /**
  * {@literal @}ProvidesWithAdvice is a Guice usage pattern whereby a method of a module can be 
  * annotated with {@literal @}ProvidesWithAdvice to return the initial version of an object and then
- * have {@literal @}Advice annotated methods that can modify the object.  All of this happends BEFORE
+ * have {@literal @}Advises annotated methods that can modify the object.  All of this happens BEFORE
  * the final object is injected.  This functionality is useful for frameworks that wish to provide
  * a default implementation of a type but allow extensions and customizations by installing modules
- * with {@literal @}Advice annotated methods.
+ * with {@literal @}Advises annotated methods.
  * 
  * For example,
  *
  * <pre>
  * {@literal @}ProvidesWithAdvice
- * List<String> provideFoo() { return Arrays.asList("a", "b", "c"); }
+ * List<String> provideList() { return new ArrayList<>(Arrays.asList("a", "b", "c")); }
  *
- * {@literal @}Advice
- * ProvisionAdvice<List<String>> addMoreToFoo() { return list -> list.add("d"); }
+ * {@literal @}Advises
+ * UnaryOperator<List<String>> addMoreToList() { return list -> list.add("d"); }
  * </pre>
  *
  * will add "d" to the original ["a", "b", "c"].  When List&ltString&gt is finally injected it'll have
  * ["a", "b", "c", "d"].
  * 
  * Note that {@literal @}ProvideWithAdvice can be used with qualifiers such as {@literal @}Named 
- * with matching qualifiers added to the {@literal @}Advice methods.
+ * with matching qualifiers added to the {@literal @}Advises methods.
  * 
  */
 @Documented 
