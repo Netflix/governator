9624057b88ba4f761f4459fdbb9f83da0f0fb21e
Eran Landau <elandau@netflix.com>
2016-11-16 11:44:15 -0800

    Add more details about using @Context to warning Improve unit test.

--- governator-jersey/src/main/java/com/netflix/governator/guice/jersey/GovernatorServletContainer.java
+++ governator-jersey/src/main/java/com/netflix/governator/guice/jersey/GovernatorServletContainer.java
@@ -55,7 +55,7 @@ public class GovernatorServletContainer extends ServletContainer {
         for (Class<?> resource : config.getRootResourceClasses()) {
         	if (resource.isAnnotationPresent(com.google.inject.Singleton.class) 
     			|| resource.isAnnotationPresent(javax.inject.Singleton.class)) {
-        	    LOG.warn("Class {} should be annotated with Jersey's com.sun.jersey.spi.resource.Singleton", resource);
+        	    LOG.warn("Class {} should be annotated with Jersey's com.sun.jersey.spi.resource.Singleton.  Also make sure that any JAX-RS clasese (such as UriInfo) are injected using Jersey's @Context instead of @Inject.", resource);
         	}
         }
     }
--- governator-jersey/src/test/java/com/netflix/governator/guice/jersey/JerseyServerTest.java
+++ governator-jersey/src/test/java/com/netflix/governator/guice/jersey/JerseyServerTest.java
@@ -1,6 +1,8 @@
 package com.netflix.governator.guice.jersey;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Proxy;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -12,12 +14,16 @@ import javax.inject.Named;
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.google.common.io.CharStreams;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.util.Modules;
@@ -158,6 +164,9 @@ public class JerseyServerTest {
         @Inject
         private String foo;
         
+        @Context
+        UriInfo uri;
+        
         @GET
         public String get() {
             return foo;
@@ -209,12 +218,19 @@ public class JerseyServerTest {
             
             System.out.println("Listening on port : "+ port);
             
-            for (int i = 0; i < 2; i++) {
-                URL url = new URL(String.format("http://localhost:%d/", port));
+            URL url = new URL(String.format("http://localhost:%d/", port));
+            {
                 HttpURLConnection conn = (HttpURLConnection)url.openConnection();
                 Assert.assertEquals(200,  conn.getResponseCode());
                 
                 Assert.assertEquals(1, FieldInjectionResource.createCount);
+            }
+            
+            {
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                Assert.assertEquals(200,  conn.getResponseCode());
+                
+                Assert.assertEquals(2, FieldInjectionResource.createCount);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -222,4 +238,81 @@ public class JerseyServerTest {
         }
     }
     
+    @Path("/")
+    @com.sun.jersey.spi.resource.Singleton
+    public static class UriContextIsRequestScoped {
+        @Context
+        UriInfo uri;
+        
+        @GET
+        @Path("{subResources:.*}")
+        public String get(@PathParam("subResources") String subResources) {
+            Assert.assertTrue(Proxy.isProxyClass(uri.getClass()));
+            return uri.getPath();
+        }
+    }
+    
+    @Test
+    public void confirmUriInfoContextIsRequestScoped() {
+        // Create the injector and autostart Jetty
+        try (LifecycleInjector injector = InjectorBuilder.fromModules(
+                new ShutdownHookModule(), 
+                new GovernatorJerseySupportModule(),
+                new JerseyServletModule() {
+                    protected void configureServlets() {
+                        serve("/*").with(GovernatorServletContainer.class);
+                        
+                        bind(String.class).toInstance("foo");
+                    }
+                    
+                    @Advises
+                    @Singleton
+                    @Named("governator")
+                    UnaryOperator<DefaultResourceConfig> getResourceConfig() {
+                        return config -> {
+                            config.getClasses().add(UriContextIsRequestScoped.class);
+                            return config;
+                        };
+                    }
+                },
+                Modules.override(new JettyModule())
+                       .with(new AbstractModule() {
+                            @Override
+                            protected void configure() {
+                            }
+                            
+                            @Provides
+                            JettyConfig getConfig() {
+                                // Use emphemeral ports
+                                return new DefaultJettyConfig().setPort(0);
+                            }
+                        }))
+                        .createInjector()) {
+            
+            Assert.assertEquals(0, FieldInjectionResource.createCount);
+            
+            // Determine the emphermal port from jetty
+            Server server = injector.getInstance(Server.class);
+            int port = ((ServerConnector)server.getConnectors()[0]).getLocalPort();
+            
+            System.out.println("Listening on port : "+ port);
+            
+            {
+                URL url = new URL(String.format("http://localhost:%d/path1", port));
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                Assert.assertEquals(200,  conn.getResponseCode());
+                Assert.assertEquals("path1", CharStreams.toString(new InputStreamReader(conn.getInputStream())));
+            }
+            
+            {
+                URL url = new URL(String.format("http://localhost:%d/path2", port));
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                Assert.assertEquals(200,  conn.getResponseCode());
+                Assert.assertEquals("path2", CharStreams.toString(new InputStreamReader(conn.getInputStream())));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
 }
