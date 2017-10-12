4d8aa9faad6eb0e90e87412a28cbce6e8dc63f93
Tom Cellucci <tcellucci@netflix.com>
2017-10-12 14:34:51 -0700

    fix whitespace

--- governator-jetty/src/main/java/com/netflix/governator/guice/jetty/JettyConfig.java
+++ governator-jetty/src/main/java/com/netflix/governator/guice/jetty/JettyConfig.java
@@ -2,9 +2,9 @@ package com.netflix.governator.guice.jetty;
 
 public interface JettyConfig {
 
-	String getHost();
+    String getHost();
 	
-	int getPort();
+    int getPort();
 
     /**
      * @deprecated 2016-10-14 use {@link #getStaticResourceBase()} instead
